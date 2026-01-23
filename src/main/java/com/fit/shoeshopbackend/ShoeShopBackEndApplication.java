54fc8a330e1d2623bfafbf146ea9a36a689c8a4f
Hachimandev <nguyentrungquyen2412@gmail.com>
2026-01-23 20:55:27 +0700

    feat: Tao project shoe shop back end

---
+++ src/main/java/com/fit/shoeshopbackend/ShoeShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.fit.shoeshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeShopBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeShopBackEndApplication.class, args);
+    }
+
+}
