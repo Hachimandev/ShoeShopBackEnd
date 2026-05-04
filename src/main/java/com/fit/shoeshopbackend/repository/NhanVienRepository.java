d87cfc47a8e4b9654d359d755a6bc458ba47b6bd
Hachimandev <nguyentrungquyen2412@gmail.com>
2026-05-04 23:05:58 +0700

    feat: CRUD nhan vien

---
+++ src/main/java/com/fit/shoeshopbackend/repository/NhanVienRepository.java
@@ -0,0 +1,8 @@
+package com.fit.shoeshopbackend.repository;
+
+import com.fit.shoeshopbackend.model.NhanVien;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NhanVienRepository extends JpaRepository<NhanVien, String> {
+}
+
