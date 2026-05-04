8ed778647e86d43f06edaebb9bc52c6d1115fe24
Hachimandev <nguyentrungquyen2412@gmail.com>
2026-05-04 23:02:18 +0700

    feat: CRUD nha cung cap

---
+++ src/main/java/com/fit/shoeshopbackend/repository/NhaCungCapRepository.java
@@ -0,0 +1,8 @@
+package com.fit.shoeshopbackend.repository;
+
+
+import com.fit.shoeshopbackend.model.NhaCungCap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NhaCungCapRepository extends JpaRepository<NhaCungCap, String> {
+}
